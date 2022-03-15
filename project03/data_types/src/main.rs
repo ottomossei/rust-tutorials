3f734c96d43938c83f1c2bab885de727cbda14a6
ottomossei <seki.jobhunting@gmail.com>
2022-03-16 00:21:12 +0900

    Add array

--- project03/data_types/src/main.rs
+++ project03/data_types/src/main.rs
@@ -1,7 +1,10 @@
 mod presentation01_tuple;
+mod presentation02_array;
 
 fn main() {
     // presentation01_tuple::main01();
     // presentation01_tuple::main02();
     presentation01_tuple::main03();
+    presentation02_array::main01();
+    presentation02_array::main02();
 }
---
+++ project03/data_types/src/presentation02_array.rs
@@ -0,0 +1,23 @@
+#[allow(dead_code)]
+pub fn main01() {
+    println!("Presentation02-1");
+    let a = [1, 2, 3, 4, 5];
+
+    let mut first = a[0];
+    let second = a[1];
+    println!("first : {}", first);
+    println!("second : {}", second);
+
+    first = 9;
+    println!("first : {}, a[0]: {}", first, a[0]);
+}
+
+#[allow(dead_code)]
+pub fn main02() {
+    let a = [1, 2, 3, 4, 5];
+    let index = 10;
+
+    let element = a[index];
+
+    println!("The value of element is: {}", element); //panic
+}
